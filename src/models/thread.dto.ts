b65bc7d3ce5b84ac2e40cb11444d0264872843ec
tkgstrator <nasawake.am@gmail.com>
2025-03-28 12:34:33 +0900

    feat: update packages (#6)
    * chore(devcontainer): update configuration and scripts for development setup
    
    * feat(api): enhance thread management and OpenAPI integration
    
    * chore(vscode): enable gitdoc extension in settings
    
    * fix(error-handling): replace error cause with error name in response

--- src/models/thread.dto.ts
+++ src/models/thread.dto.ts
@@ -2,32 +2,76 @@ import { Model } from '@/enums/model'
 import { Role } from '@/enums/role'
 import { z } from 'zod'
 
-export const PromptSchema = z.object({
-  content: z.string().min(1).openapi({
-    example: 'おはようございます'
-  }),
-  role: z.nativeEnum(Role).openapi({
-    example: Role.ASSISTANT
+export const Prompt = z
+  .object({
+    role: z.nativeEnum(Role).default(Role.USER).openapi({
+      description: 'Role.',
+      example: Role.USER
+    }),
+    content: z.string().openapi({
+      description: 'Given prompt.',
+      example: 'Hello, world!'
+    })
   })
-})
+  .openapi('Prompt')
 
-export const ThreadSchema = z.object({
-  model: z.nativeEnum(Model).openapi({
-    example: Model.GPT_4O_MINI
-  }),
-  thread_id: z.string().openapi({
-    example: '1276016528223109174'
-  }),
-  is_private: z.boolean().openapi({
-    example: false
-  }),
-  discord_user_id: z.string().openapi({
-    example: '430364540899819520'
-  }),
-  prompts: z.array(PromptSchema).openapi({
-    example: []
+export const Thread = z
+  .object({
+    model: z.nativeEnum(Model).openapi({
+      description: 'The type of model.',
+      example: Model.GPT_4O_MINI
+    }),
+    thread_id: z.string().openapi({
+      description: 'Thread ID.',
+      example: '1276016528223109174'
+    }),
+    is_private: z.boolean().default(false).openapi({
+      description: 'Thread is private or not.',
+      example: false
+    }),
+    discord_user_id: z.string().openapi({
+      description: 'Discord user ID.',
+      example: '430364540899819520'
+    }),
+    prompts: z.array(Prompt).openapi({
+      description: 'List of prompts.',
+      example: [
+        {
+          role: Role.USER,
+          content: 'Hello, world!'
+        }
+      ]
+    })
   })
-})
+  .openapi('Thread')
 
-export type PromptSchema = z.infer<typeof PromptSchema>
-export type ThreadSchema = z.infer<typeof ThreadSchema>
+export const ThreadParam = z
+  .object({
+    thread_id: z.string().openapi({
+      description: 'Thread ID.',
+      example: '1276016528223109174'
+    })
+  })
+  .openapi('ThreadParam')
+
+export const ThreadQuery = z
+  .object({
+    limit: z.number().min(1).max(100).optional().default(10).openapi({
+      description: 'Limit of threads.',
+      example: 10
+    })
+  })
+  .openapi('ThreadQuery')
+
+export const ThreadPatchBody = z
+  .object({
+    discord_user_id: z.string(),
+    prompt: Prompt
+  })
+  .openapi('ThreadPatchBody')
+
+export type Prompt = z.infer<typeof Prompt>
+export type Thread = z.infer<typeof Thread>
+export type ThreadParam = z.infer<typeof ThreadParam>
+export type ThreadQuery = z.infer<typeof ThreadQuery>
+export type ThreadPatchBody = z.infer<typeof ThreadPatchBody>
