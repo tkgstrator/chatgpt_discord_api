9e05ac8836f389d46a70d7bee9721d0f77914c2c
tkgstrator <nasawake.am@gmail.com>
2025-03-28 03:27:41 +0000

    feat(api): enhance thread management and OpenAPI integration

--- src/enums/model.ts
+++ src/enums/model.ts
@@ -1,9 +1,10 @@
 export enum Model {
   GPT_35_TURBO = 'gpt-3.5-turbo',
-  GPT_4 = 'gpt-4',
-  GPT_4_0613 = 'gpt-4-0613',
-  GPT_4_0314 = 'gpt-4-0314',
-  GPT_4_TURBO = 'gpt-4-turbo',
+  GPT_45_PREVIEW = 'gpt-4.5-preview',
   GPT_4O = 'gpt-4o',
-  GPT_4O_MINI = 'gpt-4o-mini'
+  GPT_4O_MINI = 'gpt-4o-mini',
+  GPT_O1 = 'o1',
+  GPT_O1_MINI = 'o1-mini',
+  GPT_O1_PRO = 'o1-pro',
+  GPT_O3_MINI = 'o3-mini'
 }
--- src/index.ts
+++ src/index.ts
@@ -1,31 +1,61 @@
 import { OpenAPIHono as Hono } from '@hono/zod-openapi'
 import { apiReference } from '@scalar/hono-api-reference'
+import dayjs from 'dayjs'
+import customParseFormat from 'dayjs/plugin/customParseFormat'
+import timezone from 'dayjs/plugin/timezone'
+import utc from 'dayjs/plugin/utc'
+import { Context, Next } from 'hono'
+import { cache } from 'hono/cache'
 import { compress } from 'hono/compress'
 import { cors } from 'hono/cors'
 import { csrf } from 'hono/csrf'
-import { logger } from 'hono/logger'
-
 import { HTTPException } from 'hono/http-exception'
+import { logger } from 'hono/logger'
+import { timeout } from 'hono/timeout'
+import { ZodError } from 'zod'
 import { app as threads } from './threads'
 import type { Bindings } from './utils/bindings'
 import { reference, specification } from './utils/openapi'
 
-const app = new Hono<{ Bindings: Bindings }>()
+dayjs.extend(utc)
+dayjs.extend(timezone)
+dayjs.extend(customParseFormat)
+dayjs.tz.setDefault('Asia/Tokyo')
 
+const app = new Hono<{ Bindings: Bindings }>()
+app.openAPIRegistry.registerComponent('securitySchemes', 'Bearer', {
+  type: 'http',
+  scheme: 'bearer',
+  in: 'header',
+  description: 'Bearer Token'
+})
+
+app.use('*', timeout(5000))
 app.use(logger())
 app.use(csrf())
 app.use(cors())
 app.use(compress({ encoding: 'deflate' }))
-app.onError((error, c) => {
+app.use('*', (c, next) => {
+  if (new URL(c.req.url).hostname !== 'localhost') {
+    cache({ cacheName: 'discord_stripe_store', cacheControl: 'public, max-age=3600' })
+  }
+  return next()
+})
+app.onError(async (error, c) => {
   if (error instanceof HTTPException) {
     return c.json({ message: error.message }, error.status)
   }
-  return c.json({ message: 'Internal Server Error' }, 500)
+  if (error instanceof ZodError) {
+    return c.json({ message: JSON.parse(error.message), description: error.cause }, 400)
+  }
+  console.error(error)
+  return c.json({ message: error.message }, 500)
 })
-
-app.doc('/specification', specification)
-app.get('/docs', apiReference(reference))
 app.route('/threads', threads)
+app.doc31('/openapi.json', specification)
+// app.getOpenAPI31Document(specification)
+app.get('/docs', apiReference(reference))
+app.notFound((c) => c.redirect('/docs'))
 
 export default {
   port: 3000,
